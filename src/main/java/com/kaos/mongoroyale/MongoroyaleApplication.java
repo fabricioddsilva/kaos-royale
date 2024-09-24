707803556e519bd386aec1c9b36c05a78472abd5
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-09-23 21:57:21 -0300

    Projeto Criado

---
+++ src/main/java/com/kaos/mongoroyale/MongoroyaleApplication.java
@@ -0,0 +1,13 @@
+package com.kaos.mongoroyale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoroyaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoroyaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaos/mongoroyale/MongoroyaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaos.mongoroyale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoroyaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
